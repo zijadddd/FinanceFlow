337498003f49f801b10c665a281a7be5c20479ee
Zijad Doglod <zijaddoglod01@gmail.com>
2024-08-14 18:57:19 +0200

    2 attributes added to the Transaction DTO

--- financeflow-be/src/main/java/com/financeflow/be/models/dto/TransactionOut.java
+++ financeflow-be/src/main/java/com/financeflow/be/models/dto/TransactionOut.java
@@ -8,17 +8,21 @@ import lombok.Data;
 public class TransactionOut {
     private String accountName;
     private String description;
+    private String expense;
     private Double amount;
     private String currencyCode;
     private Double defaultAmount;
     private String defaultCurrencyCode;
+    private String processedAt;
 
-    public TransactionOut(String accountName, String description, Double amount, String currencyCode) {
+    public TransactionOut(String accountName, String description, String expense, Double amount, String currencyCode, String processedAt) {
         this.accountName = accountName;
         this.description = description;
+        this.expense = expense;
         this.amount = amount;
         this.currencyCode = currencyCode;
         this.defaultAmount = amount;
         this.defaultCurrencyCode = currencyCode;
+        this.processedAt = processedAt;
     }
 }
