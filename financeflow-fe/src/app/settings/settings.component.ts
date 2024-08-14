030fdb9d7094d0a44f98f7dc834d6265d5fe68fd
Zijad Doglod <zijaddoglod01@gmail.com>
2024-08-14 19:24:56 +0200

    Default currency fetched and exchange rate updated message fetched also

--- financeflow-fe/src/app/settings/settings.component.ts
+++ financeflow-fe/src/app/settings/settings.component.ts
@@ -1,5 +1,8 @@
 import { encapsulateStyle } from '@angular/compiler';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DefaultaccountService } from '../shared/services/defaultaccount.service';
+import { DefaultAccountResponse } from '../shared/models/defaultaccount.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-settings',
@@ -9,6 +12,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrl: './settings.component.css',
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  public exchangeRateChanged: string;
+  public defaultAccount: DefaultAccountResponse;
+  private currencyApi: string = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/#.json";
+  public currencies: any;
+
+  constructor(private defaultAccountService: DefaultaccountService, private htppClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.defaultAccountService.getDefaultAccount().subscribe((response: DefaultAccountResponse) => {
+      this.defaultAccount = response;
+    });
+
+    this.htppClient.get(this.currencyApi.replace("#", this.defaultAccount.currencyCode.toLowerCase())).subscribe((response: any) => {
+      this.exchangeRateChanged = response.date;
+    });
+  }
 
 }
